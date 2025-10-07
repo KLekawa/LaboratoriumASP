cf474996ae6136ebfca8bce4ad0f57f9209a849b
kacper.lekawa <kacper.lekawa@microsoft.wsei.edu.pl>
2025-10-07 18:22:44 +0200

    Lab01 end

--- Lab0/Controllers/HomeController.cs
+++ Lab0/Controllers/HomeController.cs
@@ -23,6 +23,47 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Age(DateTime date)
+    {
+        var age = DateTime.Now.Year - date.Year;
+        ViewBag.Result = age;
+        
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Calculator(double? x, double? y, [FromQuery(Name = "operator")]string op)
+    {
+        if (x is null || y is null) return View("CalculatorError", "Brak wartościo x lub y!");
+        
+        
+        switch (op)
+        {
+            case "add":
+                ViewBag.Result = $"{x} + {y} = {x+y}";
+                break;
+            case "sub":
+                ViewBag.Result = $"{x} - {y} = {x-y}";
+                break;
+            case "mul":
+                ViewBag.Result = $"{x} * {y} = {x*y}";
+                break;
+            case "div":
+                ViewBag.Result = $"{x} / {y} = {x/y}";
+                break;
+            default:
+                ViewBag.Result = "Nieznany operator";
+                break;
+        }
+        
+        return View();
+    }
+    
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
