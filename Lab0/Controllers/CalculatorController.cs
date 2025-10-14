f880ce1f3243219aa6e30ce6aed2368c6d810005
kacper.lekawa <kacper.lekawa@microsoft.wsei.edu.pl>
2025-10-14 17:12:33 +0200

    laab02 first

---
+++ Lab0/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab0.Controllers;
+
+public class CalculatorController : Controller
+{
+    // GET
+    public IActionResult Form()
+    {
+        return View();
+    }
+}
